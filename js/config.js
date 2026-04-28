f1f9dab5341df4ea00b5ee0ab34d4ae6e38294e0
Creeperflori <154248113+Creeperflori@users.noreply.github.com>
2026-04-28 19:40:47 +0200

    Add initial configuration file for application settings

---
+++ js/config.js
@@ -0,0 +1,30 @@
+// ==========================================
+// ZENTRALE KONFIGURATION (config.js)
+// ==========================================
+
+const CONFIG = {
+    // Deine Admin-Mail für die Rechte auf der Seite
+    adminEmail: "creeper.flori44@gmail.com",
+    
+    // Alle deine Social Media & Server Links
+    links: {
+        twitch: "https://www.twitch.tv/Creeperflori",
+        youtube: "https://www.youtube.com/@creeperflori",
+        tiktok: "https://www.tiktok.com/@creeperflori",
+        instagram: "https://www.instagram.com/creeperflori",
+        creeperCave: "https://discord.gg/DEIN_NEUER_INVITE", // Platzhalter für deinen neuen Server!
+        deppenCord: "https://discord.gg/HtqwEnsb2n", // Kommt später zu den Partnern
+        serverIP: "FILLYPATH.DE",
+        serverDiscord: "https://discord.gg/Sn4mhjPNUc"
+    },
+
+    // Deine Firebase Zugangsdaten
+    firebase: {
+        apiKey: "AIzaSyAjeDZUZj87o0U5HK8WOI1kYU9Hpqcg7eA",
+        authDomain: "creeperflori-web.firebaseapp.com",
+        projectId: "creeperflori-web",
+        storageBucket: "creeperflori-web.firebasestorage.app",
+        messagingSenderId: "893993744040",
+        appId: "1:893993744040:web:e48fcefca7502054c3f49d"
+    }
+};
